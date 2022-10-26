c1833d2033670e2a2753ffcdb4dc6206991233ee
hyacinth <hyacinthweng@gmail.com>
2022-10-26 01:08:51 -0400

    new hungry pages

--- food-api/main.js
+++ food-api/main.js
@@ -4,7 +4,6 @@ recipeButton.addEventListener("click", generateRecipe);
 
 async function generateRecipe(){
     const recipeData = await retrieveData();
-    // generateHTML(recipeData);
 };
 
 async function retrieveData(){
@@ -12,12 +11,30 @@ async function retrieveData(){
     const recipeInfo = "https://api.spoonacular.com/recipes/random?apiKey=7102f32ae95747718c96a58fa5cc028b&includeNutrition=false";
     const recipeResponse = await fetch(recipeInfo);
 
-    const recipeData = await recipeResponse.json();
+    const recipeData = await recipeResponse.json(); //recipeData is JSON object
+    console.log(recipeData);
+    generateHTML(recipeData);
 
-    const recipeFull = recipeData.url;
-    console.log(recipeFull);
 };
 
+
 function generateHTML(recipeData){
-    
+    let fullRecipe = '';
+    if(recipeData.recipes[0].image){
+        let recipeImg = `<img src="${recipeData.recipes[0].image}">`;
+        fullRecipe += recipeImg;
+    }
+    if(recipeData.recipes[0].title){
+        let recipeTitle = `<h2>${recipeData.recipes[0].title}</h2>`;
+        fullRecipe += recipeTitle;
+    }
+    if(recipeData.recipes[0].servings){
+        let recipeServings = `<h3>Servings: ${recipeData.recipes[0].servings}</h3>`;
+        fullRecipe += recipeServings;
+    }
+    if(recipeData.recipes[0].instructions){
+        let recipeInstructions = `<p>${recipeData.recipes[0].instructions}</p>`;
+        fullRecipe += recipeInstructions;
+    }
+    document.querySelector("main").innerHTML = fullRecipe;
 }
